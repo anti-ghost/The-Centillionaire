1668fa50a2b364332a1d310869e439fa6ce5fa34
resu deteleD <34798985+anti-ghost@users.noreply.github.com>
2022-11-13 11:00:55 -0800

    Update script.js

--- script.js
+++ script.js
@@ -48,19 +48,19 @@ const newGame = {
 };
 
 const WORDS = [
-  "thousand",
-  "million",
-  "billion",
-  "trillion",
-  "quadrillion",
-  "quintillion",
-  "sextillion",
-  "septillion",
-  "octillion",
-  "nonillion",
-  "decillion",
-  "undecillion",
-  "duodecillion"
+  " thousand",
+  " million",
+  " billion",
+  " trillion",
+  " quadrillion",
+  " quintillion",
+  " sextillion",
+  " septillion",
+  " octillion",
+  " nonillion",
+  " decillion",
+  " undecillion",
+  " duodecillion"
 ];
 
 const LETTERS = [
